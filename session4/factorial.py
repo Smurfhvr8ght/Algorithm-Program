7572ed1802d6529a2fb316259dc1bf6abab88f59
Smurfhvr8ght <andres.winson@binus.ac.id>
2024-09-25 21:44:35 +0700

    Update factorial.py

--- session4/factorial.py
+++ session4/factorial.py
@@ -1,11 +1,11 @@
 def facy(A):
-    if A>0:
-        if A != 1:
-            return A * facy(A-1)
+    if A>0:    # make sure negative and 0 doesn't ruin the code
+        if A != 1:     #checks if A is currently not 1
+            return A * facy(A-1)  # return the value of A multiplied by A-1 and so on by looping to A-2, A-3 and so on
         else: 
-            return 1
+            return 1    #Value to return at 1 to prevent infinite loop
     else:
-        return "number can't be 0 and below"
+        return "number can't be 0 and below"    #Just some error msg
 
 #Test run below
 check = int(input("Put whole number to do factorial:"))
