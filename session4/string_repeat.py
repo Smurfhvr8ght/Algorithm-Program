86526de5a0d257d351cb39c029bfb9ac1f09cd85
Smurfhvr8ght <andres.winson@binus.ac.id>
2024-09-25 21:49:21 +0700

    Update string_repeat.py

--- session4/string_repeat.py
+++ session4/string_repeat.py
@@ -1,12 +1,12 @@
 def sr(T,S):
-    A=""
+    A=""    #To keep track of the current result
     if len(T) == len(S):
-        x=0
-        for j in S:
-            for i in range(T[x]):
-                 A += j
-            x+=1
-    return A
+        x=0    #counter to make sure that value in tupple is checked as needed
+        for j in S:    #get value of each letter in string and put it to j at different iteration
+            for i in range(T[x]):    #to loop a certain number of time base on the value of tupple from position x
+                 A += j    # put string to result variable
+            x+=1    #increase the counter
+    return A    #returen value of result
 
 #Test run below
 T = (1,2,3)
