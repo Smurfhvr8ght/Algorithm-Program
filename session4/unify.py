115158681a4efc24a8ec63fc2cdc312beed78606
Smurfhvr8ght <andres.winson@binus.ac.id>
2024-09-25 21:54:50 +0700

    Update unify.py

--- session4/unify.py
+++ session4/unify.py
@@ -1,16 +1,16 @@
 def unify(A,B):
-    B = list(dict.fromkeys(B))
-    A = list(dict.fromkeys(A))
-    C=A
-    for i in B:
-        flag = False
-        for j in range(len(A)):
-            if i == A[j]:
-                flag = True
-        if flag == False:
-            C.append(i)
-    C.sort()
-    return C
+    B = list(dict.fromkeys(B))    #to remove all duplicates in B
+    A = list(dict.fromkeys(A))    #to remove all duplicates in A
+    C=A    #put all value in A into C
+    for i in B:    #check each value of B in different iteration
+        flag = False    #declaration of flag for tracking purpose
+        for j in range(len(A)):    #Looping base on number of value in A and put an increasing counter in J for each value in A
+            if i == A[j]:    #check for same value between i and all value of j
+                flag = True    #if result true then raise flag
+        if flag == False:    #check if flag is not raised
+            C.append(i)    #put value i into C
+    C.sort()    #sort value of C in ascending for aesthetic purpose
+    return C    #give the C as result from function
 
 #Test run below
 A=[1,2,3,8,9,0]
